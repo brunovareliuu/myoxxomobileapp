f4513cc1b62d0eef9fa11fa3a1b1f1b9288d311b
brunovareliuu <varelavibruno@gmail.com>
2025-05-16 20:36:47 -0600

    ho

--- App.js
+++ App.js
@@ -1,20 +1,42 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { initializeApp } from "firebase/app";
+
+// Importar las pantallas
+import Welcome from './screens/auth/Welcome';
+import Login from './screens/auth/Login';
+import Signup from './screens/auth/Signup';
+
+// Configuración de Firebase
+const firebaseConfig = {
+  apiKey: "AIzaSyA2UPFXjD963tlAlcPB7gZyXAaRqZJWZaI",
+  authDomain: "myoxxovision.firebaseapp.com",
+  databaseURL: "https://myoxxovision-default-rtdb.firebaseio.com",
+  projectId: "myoxxovision",
+  storageBucket: "myoxxovision.firebasestorage.app",
+  messagingSenderId: "491253915189",
+  appId: "1:491253915189:web:45a8f84e556c93880c5b7c"
+};
+
+// Inicializar Firebase
+initializeApp(firebaseConfig);
+
+const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator 
+        initialRouteName="Welcome"
+        screenOptions={{
+          headerShown: false,
+        }}
+      >
+        <Stack.Screen name="Welcome" component={Welcome} />
+        <Stack.Screen name="Login" component={Login} />
+        <Stack.Screen name="Signup" component={Signup} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
